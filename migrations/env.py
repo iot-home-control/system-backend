7877e0664fabd1bbbffa0255ad30ca5d9ece072f
Daniel Schulte <trilader@schroedingers-bit.net>
2020-04-05 21:25:43 +0200

    Get sqlachemy connection string from config.py

--- migrations/env.py
+++ migrations/env.py
@@ -9,6 +9,7 @@ import sys
 import os
 sys.path.append(os.getcwd())
 from models.database import Base
+from config import SQLALCHEMY_DATABASE_URI
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -28,7 +29,7 @@ target_metadata = Base.metadata
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-
+config.set_main_option('sqlalchemy.url', SQLALCHEMY_DATABASE_URI)
 
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
