2fa72868fd4cc35a6c6c2ab2861c3a0a622871d7
Hinrikus Wolf <mail@hinrikus-wolf.de>
2020-09-13 16:42:23 +0200

    fix that system backend collects stalled websocket connections

--- main.py
+++ main.py
@@ -86,7 +86,14 @@ def timer_checker_thread():
 async def send_to_all(msg):
     if not connected_wss:
         return
-    await asyncio.wait([ws.send(msg) for ws in connected_wss])
+
+    async def sender(ws):
+        try:
+            await ws.send(msg)
+        except websockets.exceptions.ConnectionClosed:
+            pass
+
+    await asyncio.wait([sender(ws) for ws in connected_wss])
 
 
 class JsonEncoder(json.JSONEncoder):
@@ -102,64 +109,70 @@ async def handle_ws_connection(websocket, path):
     db = shared.db_session_factory()
     time.sleep(0.2)
 
-    known_things = db.query(Thing).filter_by(visible=True).order_by(Thing.id).all()
-    msg = dict(type="things", things=[t.to_dict() for t in known_things])
-    await websocket.send(json.dumps(msg))
+    try:
+        known_things = db.query(Thing).filter_by(visible=True).order_by(Thing.id).all()
+        msg = dict(type="things", things=[t.to_dict() for t in known_things])
+        await websocket.send(json.dumps(msg))
 
-    states = [s for s in (t.last_state(db) for t in known_things) if s]
-    msg = dict(type="states", states=[s.to_dict() for s in states])
-    await websocket.send(json.dumps(msg, cls=JsonEncoder))
+        states = [s for s in (t.last_state(db) for t in known_things) if s]
+        msg = dict(type="states", states=[s.to_dict() for s in states])
+        await websocket.send(json.dumps(msg, cls=JsonEncoder))
 
-    views_query = db.query(View).order_by(View.name, View.id).all()
-    views = dict(All=[thing.id for thing in known_things])
-    views.update({view.name: [thing.id for thing in view.things] for view in views_query})
-    msg = dict(type="views", views=views)
-    await websocket.send(json.dumps(msg))
-    db.close()
+        views_query = db.query(View).order_by(View.name, View.id).all()
+        views = dict(All=[thing.id for thing in known_things])
+        views.update({view.name: [thing.id for thing in view.things] for view in views_query})
+        msg = dict(type="views", views=views)
+        await websocket.send(json.dumps(msg))
+        db.close()
 
-    async for message in websocket:
-        db = shared.db_session_factory()
-        try:
-            data = json.loads(message)
-            if isinstance(data, dict):
-                msg_type = data.get("type")
-                if msg_type:
-                    if msg_type == "message":
-                        await send_to_all(json.dumps(data))
-                    elif msg_type == "command":
-                        wslog.info("Command message: {}".format(message))
-                        thing_id = data.get("id")
-                        thing = db.query(Thing).get(thing_id)
-                        if not thing:
-                            wslog.warning("Thing {} is unknown".format(thing_id))
-                            continue
-                        if thing.type in ['switch', 'shelly']:
-                            sw = db.query(Thing).get(thing_id)
-                            val = data.get("value")
-                            if val:
-                                sw.on()
+        async for message in websocket:
+            db = shared.db_session_factory()
+            try:
+                data = json.loads(message)
+                if isinstance(data, dict):
+                    msg_type = data.get("type")
+                    if msg_type:
+                        if msg_type == "message":
+                            await send_to_all(json.dumps(data))
+                        elif msg_type == "command":
+                            wslog.info("Command message: {}".format(message))
+                            thing_id = data.get("id")
+                            thing = db.query(Thing).get(thing_id)
+                            if not thing:
+                                wslog.warning("Thing {} is unknown".format(thing_id))
+                                continue
+                            if thing.type in ['switch', 'shelly']:
+                                sw = db.query(Thing).get(thing_id)
+                                val = data.get("value")
+                                if val:
+                                    sw.on()
+                                else:
+                                    sw.off()
                             else:
-                                sw.off()
+                                wslog.warning("Unsupported type for command: '{}'".format(thing.type))
+                        elif msg_type == "last_seen":
+                            things = db.query(Thing).all()
+                            last_seen = {thing.id: thing.last_seen.isoformat() if thing.last_seen else None for thing in things}
+                            msg = dict(type="last_seen", last_seen=last_seen)
+                            await websocket.send(json.dumps(msg))
                         else:
-                            wslog.warning("Unsupported type for command: '{}'".format(thing.type))
-                    elif msg_type == "last_seen":
-                        things = db.query(Thing).all()
-                        last_seen = {thing.id: thing.last_seen.isoformat() if thing.last_seen else None for thing in things}
-                        msg = dict(type="last_seen", last_seen=last_seen)
-                        await websocket.send(json.dumps(msg))
+                            wslog.warning("Unknown msg_type {}".format(msg_type))
                     else:
-                        wslog.warning("Unknown msg_type {}".format(msg_type))
+                        wslog.warning("Discarding message from {}: Missing \"type\" field".format(websocket.remote_address))
                 else:
-                    wslog.warning("Discarding message from {}: Missing \"type\" field".format(websocket.remote_address))
-            else:
-                wslog.warning("Discarding message from {}: Not a JSON object: {}".format(websocket.remote_address, data))
-        except json.JSONDecodeError as err:
-            wslog.warning("Discarding message from {}: Can't decode as JSON ({})".format(websocket.remote_address, str(err)))
-        db.close()
-    else:
-        wslog.info("Client {} disconnected".format(websocket.remote_address))
+                    wslog.warning("Discarding message from {}: Not a JSON object: {}".format(websocket.remote_address, data))
+            except json.JSONDecodeError as err:
+                wslog.warning("Discarding message from {}: Can't decode as JSON ({})".format(websocket.remote_address, str(err)))
+            db.close()
+        else:
+            wslog.info("Client {} disconnected".format(websocket.remote_address))
+            connected_wss.remove(websocket)
+            db.close()
+    except websockets.exceptions.ConnectionClosed:
         connected_wss.remove(websocket)
-        db.close()
+        wslog.warning(f"Cleaning up stale connection: {websocket.remote_address}")
+        if db:
+            db.close()
 
 
 def ws_thread(queue):
@@ -168,6 +181,7 @@ def ws_thread(queue):
     try:
         global ws_event_loop
         ws_event_loop = asyncio.new_event_loop()
+        # ws_event_loop.set_debug(True)
         asyncio.set_event_loop(ws_event_loop)
         ws_server = websockets.serve(handle_ws_connection, getattr(config, "BIND_IP", "localhost"), 8765)
         ws_event_loop.run_until_complete(ws_server)
