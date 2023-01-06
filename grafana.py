002647d742b6d9dde724b0444064aaf58468a28d
Daniel Schulte <trilader@schroedingers-bit.net>
2023-01-06 17:05:42 +0100

    grafana: Update copyright year

--- grafana.py
+++ grafana.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
-# Copyright (C) 2021 The Home Control Authors
+# Copyright (C) 2021-2023 The Home Control Authors
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
