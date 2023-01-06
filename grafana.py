e17866660f21f7024e3f0e53df69def3d2f5835d
Daniel Schulte <trilader@schroedingers-bit.net>
2023-01-06 17:05:29 +0100

    grafana: Remove previous, failed resampling attempts

--- grafana.py
+++ grafana.py
@@ -15,12 +15,10 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import datetime
-import itertools
 import json
 import math
 import os
 import threading
-import time
 from http.server import ThreadingHTTPServer, BaseHTTPRequestHandler
 from typing import Optional
 from urllib.parse import urlparse, parse_qs
@@ -92,12 +90,6 @@ class Handler(BaseHTTPRequestHandler):
                 DataType.Boolean: State.status_bool,
             }
 
-            def round_dt_to_seconds(dt: datetime.datetime) -> datetime.datetime:
-                if dt.microsecond < 50000:
-                    return dt.replace(microsecond=0)
-                else:
-                    return dt.replace(microsecond=0) + datetime.timedelta(seconds=1)
-
             with db_session_factory() as db:
                 resp = []
                 for target in targets:
@@ -127,7 +119,6 @@ class Handler(BaseHTTPRequestHandler):
                         states_query = states_query.limit(1)
 
                     states = db.execute(states_query)
-                    # print("Found", trends.count(), "trends for", thing.name, thing.type, thing.id, "between", range_start, "and", range_stop)
                     for trend in trends.all():
                         when = round((trend.start + trend.interval/2).timestamp()*1000)
                         if trend.t_avg is None or math.isnan(trend.t_avg):
@@ -145,9 +136,6 @@ class Handler(BaseHTTPRequestHandler):
                         print("no dp")
                         resp.append(dict(target=display_name, datapoints=datapoints))
 
-
-                    #interval_points = [range_start + datetime.timedelta(milliseconds=interval_ms * i) for i in
-                    #                   range(round((range_stop-range_start).total_seconds()*1000 / interval_ms))]
                     interval_points = []
 
                     duration_td = datetime.timedelta(milliseconds=interval_ms)
@@ -200,105 +188,6 @@ class Handler(BaseHTTPRequestHandler):
                         resampled.append([v, round(current_interval.timestamp() * 1000)])
 
                         interval_index += 1
-
-                    """
-                    interval_index = 0
-                    start = time.time()
-                    print(start)
-
-                    for left_dp, right_dp in itertools.pairwise(datapoints):
-
-                        for interval in range(interval_index, len(interval_points)):
-                            current_interval = interval_points[interval]
-                            #x = time.time()
-                            if current_interval[0] <= left_dp[1]:
-                                if left_dp[1] < current_interval[1] and current_interval[1] <= right_dp[1] < current_interval[2]:
-                                    #print("y", time.time() -x )
-                                    timediff = right_dp[1] - left_dp[1]
-                                    #assert timediff.total_seconds() > 0
-                                    f = (current_interval[1] - left_dp[1]) / timediff
-                                    #assert f >= 0, f'{current_interval=} {left_dp[1]=} {timediff=}'
-                                    v = left_dp[0] + (right_dp[0] - left_dp[0]) * f
-                                    resampled.append([v, round(current_interval[1].timestamp() * 1000)])
-                                    #print(interval-interval_index)
-                                    interval_index = interval
-                                    break
-                            else:
-                                interval_index = interval
-                                #print("n", time.time() - x)
-                    print(f"needed: {time.time()-start} for {len(datapoints)}")
-                    """
-                    """
-                            if interval < len(interval_points) - 1 and right_dp[1] >= interval_points[interval + 1]:
-
-                            if left_dp[1] < interval_points[interval] <= right_dp[1]:
-                                interval_index = interval
-                                continue
-
-                                interval_index = interval
-                                break
-                        current_interval = interval_points[interval_index]
-                        assert left_dp[1] < current_interval <= right_dp[1], f'{left_dp[1]=} {current_interval=}, {right_dp[1]=}'
-
-                        timediff = right_dp[1] - left_dp[1]
-                        assert timediff.total_seconds() > 0
-                        f = (current_interval - left_dp[1]) / timediff
-                        assert f >= 0, f'{current_interval=} {left_dp[1]=} {timediff=}'
-                        v = left_dp[0] + (right_dp[0] - left_dp[0]) * f
-                        resampled.append([v, round(current_interval.timestamp() * 1000)])
-                        """
-                    """
-                    interval_index = 1
-                    interval_td = datetime.timedelta(milliseconds=interval_ms)
-
-                    dp_index = 0
-                    for index in range(0, len(interval_points)):
-                        items = []
-                        dp = datapoints[dp_index]
-                        new_dp = datapoints[dp_index]
-                        while index < len(interval_points)-1 and new_dp[1] < interval_points[index+1]:
-                            dp_index += 1
-                            if dp_index >= len(datapoints):
-                                break
-                            dp = new_dp
-                            new_dp = datapoints[dp_index]
-                            if new_dp[1] < interval_points[index]:
-                                continue
-                        if index >= len(interval_points)-1 or dp[1] >= interval_points[index+1]:
-                            continue
-                        assert dp != new_dp
-                        # linear intepolation
-                        assert dp[1] <= interval_points[index+1] <= new_dp[1], f'{index=} {dp[1]=}, {interval_points[index+1]=} {new_dp[1]=}'
-
-                        timediff = new_dp[1] - dp[1]
-                        f = (interval_points[index+1] - dp[1]) / timediff
-                        assert f >= 0
-                        v = dp[0] + (new_dp[0] - dp[0]) * f
-                        resampled.append([v, round(interval_points[index+1].timestamp()*1000)])
-                        """
-
-                    """
-                    for index in range(1, len(datapoints)):  # since we already added the first point to resampled
-                    dp = datapoints[index]
-                    if dp[1] < interval_points[interval_index]:
-                        continue
-                
-                    while interval_index < len(interval_points) - 1 and dp[1] > interval_points[interval_index] + interval_td:
-                        interval_index += 1
-                
-                    timediff = dp[1] - datapoints[index - 1][1]
-                    if timediff.total_seconds() == 0:
-                        resampled.append([value, round(interval_points[interval_index].timestamp() * 1000)])
-                        continue
-                    f = (interval_points[interval_index] - datapoints[index - 1][1]) / timediff
-                    value = datapoints[index - 1][0] + (dp[0] - datapoints[index - 1][0]) * f
-                    if value < 0:
-                        print(timediff, f, value, datapoints[index - 1][0], dp[0], datapoints[index - 1][1],
-                              interval_points[interval_index])
-                    resampled.append([value, round(interval_points[interval_index].timestamp() * 1000)])
-                    """
-
-                    # resampled.append([dp[0], round(interval_points[interval_index].timestamp()*1000)])
 
                     print(interval_ms, len(resampled), "<-", len(datapoints), max_data_points)
                     resp.append(dict(target=display_name, datapoints=resampled))
